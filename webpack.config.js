4efe17f19e4d1a48e80cde17ba360b70a4c0acd4
Mirtau <tjj72@comcast.net>
2018-03-06 15:42:52 -0700

    still working on dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {resolve} = require('path')
-const srcDir = resolve(__dirname, 'src')
-
-
+const path = require('path')
+var config = require('./webpack.config.js')
+var webpack = require('webpack')
+//const srcDir = resolve(__dirname, 'src')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js'
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'bundle.js'
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  port: 9000
+},
   plugins: [new HtmlWebpackPlugin()]
 
 }
